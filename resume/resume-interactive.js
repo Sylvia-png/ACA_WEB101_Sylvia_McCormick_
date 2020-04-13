bccc3e5982144c3b81cac24134114e2fb6c67ea4
sylvia <ojitoscecy.com>
2020-04-12 21:50:05 -0500

    adding interactive

---
+++ resume/resume-interactive.js
@@ -0,0 +1,15 @@
+// Resume title
+const animateName = (element) => {
+    element.classList.add("animated", "infinite", "flash")
+  }
+  
+  const animateNumber = (element) => {
+    element.innerHTML = "125-683-9824";
+    element.href = "125-683-9824";
+   
+  }
+  
+  const animatePhoto = (element) => {
+    element.classList.add("animated", "infinite", "flash")
+  }
+  
